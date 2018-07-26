d31faa88e0ef33a1a967d79fceaaa459e78a384c
Vitche Research Team Developer <developer@vitche.com>
2018-07-26 13:29:12 +0000

    1. Do not create a client when no settings are available; 2. Do not override console methods when no client is available.

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 var util = require("util");
 var syslog = require("syslog-client");
-var client = syslog.createClient(process.env.SYSLOG_ADDRESS, {
-	syslogHostname: process.env.SYSLOG_COMPONENT_NAME,
-	transport: syslog.Transport.Udp,
-	port: process.env.SYSLOG_PORT
-});
+var client = undefined;
+var serverAddress = process.env.SYSLOG_ADDRESS;
+var serverPort = process.env.SYSLOG_PORT;
+var componentName = process.env.SYSLOG_COMPONENT_NAME;
+if (serverAddress && serverPort && componentName) {
+	client = syslog.createClient(serverAddress, {
+		syslogHostname: componentName,
+		transport: syslog.Transport.Udp,
+		port: serverPort
+	});
+}
 module.exports.log = function() {
 	var message = util.format.apply(null, arguments);
 	client.log(message, {
@@ -24,8 +30,14 @@ module.exports.log = function() {
 	});
 };
 module.exports.replaceConsoleError = function () {
+	if(!client) {
+		return;
+	}
 	console.error = module.exports.log;
 };
 module.exports.replaceConsoleLog = function () {
+	if(!client) {
+		return;
+	}
 	console.log = module.exports.log;
 };
